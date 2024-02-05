a02c2e5c348e2b319c893e57e2e4eb16a05d449b
Dulniko <jakub.dulnik03@gmail.com>
2024-02-05 19:17:31 +0100

    spectacular settings

--- library/catalog/serializers.py
+++ library/catalog/serializers.py
@@ -4,24 +4,28 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = "__all__"
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Author
         fields = "__all__"
 
 
 class BookSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Book
         fields = "__all__"
 
 
 class PublisherSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Publisher
         fields = "__all__"
--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -2,12 +2,11 @@ from django.urls import path, include
 from catalog.viewsets import UserViewSet, AuthorViewSet, BookViewSet, PublisherViewSet
 from rest_framework.routers import SimpleRouter
 
-
 router = SimpleRouter()
 router.register(r"users", UserViewSet, basename="user")
 router.register(r"authors", AuthorViewSet, basename="author")
 router.register(r"books", BookViewSet, basename="book")
-router.register(r"publishers", PublisherViewSet, basename="publisher") 
+router.register(r"publishers", PublisherViewSet, basename="publisher")
 
 urlpatterns = [
     path("", include(router.urls)),

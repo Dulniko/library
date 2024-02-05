f4963ca80e9b4a2b3807c8d1a6e8e88487277353
Dulniko <dulnik@DESKTOP-BFRCU8D>
2024-02-05 21:02:02 +0100

    add schema urls

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("catalog/", include("catalog.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]

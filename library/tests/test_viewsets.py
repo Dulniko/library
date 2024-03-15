6922192aaf5c311f826e30cb2c91e1c5759e4225
Dulniko <jakub.dulnik03@gmail.com>
2024-03-15 16:29:17 +0100

    author detail test

--- library/tests/test_viewsets.py
+++ library/tests/test_viewsets.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 from tests.helpers import load_fixture
+# from django.contrib.auth.models import User
 
 
 @pytest.mark.django_db
@@ -38,3 +39,24 @@ def test_author_list(api_client):
             "followers": [],
         },
     ]
+
+
+@pytest.mark.django_db
+@load_fixture(("tests/fixtures/authors.yaml", ))
+def test_author_detail(api_client):
+    url = reverse("author-detail", kwargs={"pk": 1})
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == {
+        "id": 1,
+        "firstname": "Andrzej",
+        "lastname": "Sapkowski",
+        "alive": True,
+        "address": "Łódź",
+        "zipcode": "90-008",
+        "telephone": "123123123",
+        "recommendedby": None,
+        "joindate": "2024-01-20",
+        "popularity_score": 80,
+        "followers": [],
+    }

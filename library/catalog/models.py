e5076382ee9c3b5a6500c53c923894a45d6ea229
Dulniko <jakub.dulnik03@gmail.com>
2024-04-04 19:24:40 +0200

    models up

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,5 +1,15 @@
+from django.core.validators import RegexValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message=
+    "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+)
+zipcode_regex = RegexValidator(
+    regex=r'^\d{5}$', message="Zip code must be composed of 5 digits.")
 
 
 class Author(models.Model):
@@ -7,23 +17,28 @@ class Author(models.Model):
     lastname = models.CharField(max_length=100)
     alive = models.BooleanField()
     address = models.CharField(max_length=200, null=True, blank=True)
-    zipcode = models.CharField(max_length=100, null=True, blank=True)
-    telephone = models.CharField(max_length=100, null=True, blank=True)
-    recommendedby = models.ForeignKey(
-        "Author",
-        on_delete=models.CASCADE,
-        related_name="recommended_authors",
-        null=True,
-        blank=True,
-    )
-    joindate = models.DateField()
-    popularity_score = models.IntegerField(blank=True, null=True)
+    zipcode = models.CharField(max_length=5,
+                               validators=[zipcode_regex],
+                               null=True,
+                               blank=True)
+    telephone = models.CharField(unique=True,
+                                 null=True,
+                                 blank=True,
+                                 validators=[phone_regex])
+    recommendedby = models.ForeignKey("self",
+                                      on_delete=models.CASCADE,
+                                      related_name="recommended_authors",
+                                      null=True,
+                                      blank=True)
+    joindate = models.DateField(
+        validators=[MaxValueValidator(limit_value=timezone.now)])
+    popularity_score = models.PositiveIntegerField(blank=True, null=True)
     followers = models.ManyToManyField(User,
                                        related_name="followed_authors",
                                        blank=True)
 
     def __str__(self):
-        return self.firstname + " " + self.lastname
+        return f"{self.firstname} {self.lastname}"
 
 
 class Book(models.Model):
@@ -49,7 +64,7 @@ class Book(models.Model):
 class Publisher(models.Model):
     name = models.CharField(max_length=100)
     joindate = models.DateField()
-    popularity_score = models.IntegerField(blank=True, null=True)
+    popularity_score = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
